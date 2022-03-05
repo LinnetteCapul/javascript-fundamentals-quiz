682578978eda6eaf2a800f5d7f7f6b7a35691c11
LinnetteCapul <linnette.capul@gmail.com>
2022-03-05 09:00:35 -0800

    added some styling to web application and deleted pseudo code in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,3 @@
-// make branches for hw
-// timed quiz - need to include timer
-    // timeer needs to be displayed - count variable?
-// need a start button for quiz
-    // eventListenerbtn to start quiz timer
-// store high scores after each quiz
-// what info is needed in the quiz
-    // questions, choices for questions, correct answer for questions
-
-// when quiz starts, needs to display first question with possible choices (user clicks on option -> more buttons for each choice)
-    // when wrong answer clicked -> needs to show/ prompt that answer is incorrect -> show display message ex: "Incorrect answer"
-
-// when user clicks on choice (whether right or wrong), it will move onto the next question
-    // as well as deduct time from incorrect answer
-    // if correct, no time deducted
-
- // when all questions are answered, quiz is finished 
-    // display/ show to user that quiz is finished. use prompt? -> message "Quiz is finished"
-    // or when timer reaches 0, quiz is finished -> message with ex: "Time's up!"
-    // show time left (if any) and score
-        // allow user to input their initials for their score
-            // need btn to submit score and initials into quiz -> local storage
- 
-// have way to store high score with initials -> local storage
-
-// need questions in var -> array
-    // need to connect each question with possible choices -> <ul> <li>
-        // need to connect correct answer with question
-        // how to group together -> objects?
-
-// need a start function for quiz
-    // start timer when start button is clicked 
-
-
 //variables for elements
 var startQuizDiv = document.getElementById("start-page");
 var startQuizButton = document.getElementById("start-btn");
